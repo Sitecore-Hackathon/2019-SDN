2b1653cd40c13ae63ff4f00f585315a5934dd84d
Niclas Bock <n.bock@kuehlhaus.com>
2019-03-02 17:02:13 +0100

    [TASK / CLEANUP] Add DeeplClient and cleanup DeeplConnector

---
+++ src/Foundation/DeeplConnector/code/DeeplClient.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace DeeplConnector {
+
+    public class DeeplClient {
+
+        private readonly string _baseUrl;
+
+        private readonly string _authKey;
+
+        public DeeplClient(string baseUrl, string authKey) {
+            _baseUrl = baseUrl;
+            _authKey = authKey;
+        }
+        
+        public string SendTranslationRequest(string text, string sourceLanguage, string targetLanguage) {
+            var request = (HttpWebRequest)WebRequest.Create(_baseUrl);
+            request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version11;
+            request.Method = "POST";
+
+            var postBytes = Encoding.UTF8.GetBytes($"auth_key={_authKey}&text={HttpUtility.UrlEncode(text)}&source_lang={HttpUtility.UrlEncode(sourceLanguage)}&target_lang={HttpUtility.UrlEncode(targetLanguage)}");
+
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            request.Accept = "application/json";
+            request.ContentLength = postBytes.Length;
+
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+
+            var response = (HttpWebResponse)request.GetResponse();
+            using (var reader = new StreamReader(response.GetResponseStream())) {
+                return reader.ReadToEnd();
+            }
+        }
+
+    }
+
+}
---
+++ src/Foundation/DeeplConnector/tests/Models/HttpBinResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Hackathon.SDN.Foundation.DeeplConnector.Tests.Models {
+
+    internal class HttpBinResponse {
+
+        [JsonProperty("data")]
+        public string Data { get; set; }
+    
+        [JsonProperty("form")]
+        public Form Form { get; set; }
+
+        [JsonProperty("headers")]
+        public Headers Headers { get; set; }
+
+        [JsonProperty("json")]
+        public object Json { get; set; }
+
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("origin")]
+        public string Origin { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+    }
+
+    internal class Form {
+
+        [JsonProperty("auth_key")]
+        public string AuthKey { get; set; }
+
+        [JsonProperty("source_lang")]
+        public string SourceLang { get; set; }
+
+        [JsonProperty("target_lang")]
+        public string TargetLang { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+    }
+
+    internal class Headers {
+
+        [JsonProperty("Accept")]
+        public string Accept { get; set; }
+
+    }
+
+}
+
